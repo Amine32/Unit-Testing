ccfca4ca9665929f0a102d0d56bcbfb5ebc6d160
Amine32 <aminov.02@hotmail.com>
2022-04-09 02:57:50 +0700

    Fixed Testing Fromat Added single digit test Added digit limitation test Added consecutive number sequence test

--- Task1/solution.js
+++ Task1/solution.js
@@ -1,6 +1,41 @@
 function order(words) {
     words = words.split(' ');
 
+    //Testing concerns non empty strings
+    if (words != "") {
+
+        //Checking each word contains only one digit
+        for (var i = 0; i < words.length; i++) {
+            var match = [];
+            match = words[i].match(/\d/g);
+            if (match == null) {
+                if (words[i] != "")
+                    throw "Word must contain a single digit";
+            } else if (match.length != 1)
+                throw "Word must contain a single digit";
+        }
+
+        //Checking no word has digit 0
+        for (var i = 0; i < words.length; i++) {
+            var match = words[i].match(/\d/);
+            if (match == 0)
+                throw "Word can only have digits from 1 to 9";
+        }
+
+        //Checking numbers are consecutive
+        var matches = [];
+        for (var i = 0; i < words.length; i++) {
+            matches.push(words[i].match(/\d/));
+        }
+        matches.sort((a, b) => a - b);
+
+        for (var i = 0; i < matches.length; i++) {
+            if (matches[i] != i + 1)
+                throw "The words in the input String will only contain valid consecutive numbers starting with 1.";
+        }
+    }
+
+    //some useful code
     var result = [];
     result.length = words.length;
 
--- Task1/solution.test.js
+++ Task1/solution.test.js
@@ -1,7 +1,57 @@
 const order = require('./solution')
 
-test('should sort given string numerically', () => {
-    expect(order("од2ин Эт1о 4многих тесто5в из3")).toStrictEqual("Эт1о од2ин из3 4многих тесто5в");
-    expect(order("frien3ds 1We ea5t pi6e ar2e let's4")).toStrictEqual("1We ar2e frien3ds let's4 ea5t pi6e");
-    expect(order("")).toStrictEqual(""); //пустой ввод должен возвращать пустую строку
+describe('order input control', () => {
+    it.each([
+        ["test"],
+        ["."],
+        ["wh1at tes1t2"],
+        ["tes12t"],
+    ])(
+        'should only allow single digited words',
+        (string) => {
+            expect(() => {
+                order(string);
+            }).toThrow("Word must contain a single digit");
+        }
+    );
+
+    it.each([
+        ["tes0t"],
+        ["wha1t tes0t"],
+    ])(
+        'should only accept digits from 1 to 9',
+        (string) => {
+            expect(() => {
+                order(string);
+            }).toThrow("Word can only have digits from 1 to 9");
+        }
+    );
+
+    it.each([
+        ["tes2t"],
+        ["a1 ver2y 4useful sent5ence"],
+    ])(
+        'should only accept consecutive digits starting from 1',
+        (string) => {
+            expect(() => {
+                order(string);
+            }).toThrow("The words in the input String will only contain valid consecutive numbers starting with 1.");
+        }
+    );
+});
+
+describe('order output control', () => {
+    it.each([
+        ["од2ин Эт1о 4многих тесто5в из3", "Эт1о од2ин из3 4многих тесто5в"],
+        ["frien3ds 1We ea5t pi6e ar2e let's4", "1We ar2e frien3ds let's4 ea5t pi6e"]
+    ])(
+        'should sort given string numerically',
+        (input, result) => {
+            expect(order(input)).toStrictEqual(result);
+        }
+    );
+
+    test('empty input should return empty string', () => {
+        expect(order("")).toStrictEqual("");
+    });
 });
